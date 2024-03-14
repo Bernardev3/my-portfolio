21700a4d23c532a573456c21a2c23e818c6313d2
Bernardev3 <118381885+Bernardev3@users.noreply.github.com>
2024-03-14 12:44:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const changeModeButton = document.getElementById("changeModeButton");
 
 changeModeButton.onclick = function() {
-	if (document.body.style.color != "whitesmoke") {
-		document.body.style.backgroundColor = "rgb(30, 30, 30)"
-		document.body.style.color = "whitesmoke"
+	if (document.body.style.color != "rgb(252, 248, 255)") {
+		document.body.style.backgroundColor = "rgb(30, 24, 36)";
+		document.body.style.color = "rgb(252, 248, 255)";
 	} else {
-		document.body.style.backgroundColor = "white"
-		document.body.style.color = "rgb(30, 30, 30)"
+		document.body.style.backgroundColor = "rgb(252, 248, 255)";
+		document.body.style.color = "rgb(30, 24, 36)";
 	}
 }
